2d00ee5fcb09e90e1faf28bf6bace71621fd9191
hotiaDiallo <ibrahimahotia.diallo@gmail.com>
2022-10-13 07:32:08 +0200

    creating our vpc and subnet

--- main.tf
+++ main.tf
@@ -1,28 +1,26 @@
-provider "aws" {}
-
-variable "cidr_blocks" {
-    description = "cidr blocks and name tags for vpc and subnets"
-    type = list(object({
-        cidr_block = string
-        name = string
-    }))
+provider "aws" {
+    region = "eu-west-3"
 }
 
+
+variable vpc_cidr_block {}
+variable subnet_1_cidr_block {}
 variable avail_zone {}
+variable env_prefix {}
 
-resource "aws_vpc" "development-vpc" {
-    cidr_block = var.cidr_blocks[0].cidr_block
-    tags = {
-        Name: var.cidr_blocks[0].name
-    }
+
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
+  tags = {
+      Name = "${var.env_prefix}-vpc"
+  }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-    vpc_id = aws_vpc.development-vpc.id
-    cidr_block = var.cidr_blocks[1].cidr_block
-    availability_zone = var.avail_zone
-    tags = {
-        Name: var.cidr_blocks[1].name
-    }
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  cidr_block = var.subnet_1_cidr_block
+  availability_zone = var.avail_zone
+  tags = {
+      Name = "${var.env_prefix}-subnet-1"
+  }
 }
-
