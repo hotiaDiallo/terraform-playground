77384d884919a3bfc8982f7acc2b9a8efa3fdea2
hotiaDiallo <ibrahimahotia.diallo@gmail.com>
2022-10-13 07:52:42 +0200

    adding our route table and internet gateway

--- main.tf
+++ main.tf
@@ -24,3 +24,47 @@ resource "aws_subnet" "myapp-subnet-1" {
       Name = "${var.env_prefix}-subnet-1"
   }
 }
+
+resource "aws_route_table" "myapp-route-table" {
+   vpc_id = aws_vpc.myapp-vpc.id
+
+   # default route, mapping VPC CIDR block to "local", created implicitly and cannot be specified.
+   route {
+     cidr_block = "0.0.0.0/0"
+     gateway_id = aws_internet_gateway.myapp-igw.id
+   }
+
+   tags = {
+     Name = "${var.env_prefix}-route-table"
+   }
+}
+
+resource "aws_internet_gateway" "myapp-igw" {
+	vpc_id = aws_vpc.myapp-vpc.id
+    
+    tags = {
+     Name = "${var.env_prefix}-internet-gateway"
+   }
+}
+
+# Associate subnet with Route Table : only when we do not use the main route table
+resource "aws_route_table_association" "a-rtb-subnet" {
+  subnet_id      = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
+}
+
+# Only if we use the main route table instead of creating a new one 
+/*
+resource "aws_default_route_table" "example" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+
+  tags = {
+     Name = "${var.env_prefix}-main-rtb"
+   }
+}
+*/
