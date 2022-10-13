b226c644081ada10e3bba7d4d49bed69263c2399
hotiaDiallo <ibrahimahotia.diallo@gmail.com>
2022-10-13 08:14:27 +0200

    configuring our sg

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ variable vpc_cidr_block {}
 variable subnet_1_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
+variable my_ip {}
 
 
 resource "aws_vpc" "myapp-vpc" {
@@ -24,6 +25,38 @@ resource "aws_subnet" "myapp-subnet-1" {
       Name = "${var.env_prefix}-subnet-1"
   }
 }
+
+resource "aws_security_group" "myapp-sg" {
+  name   = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-sg"
+  }
+}
+
 
 resource "aws_route_table" "myapp-route-table" {
    vpc_id = aws_vpc.myapp-vpc.id
